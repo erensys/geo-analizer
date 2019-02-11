f1e6812ade09bbe34901a94c08a6159db86a779b
shishirapradhan2015@gmail.com <shishirapradhan2015@gmail.com>
2019-02-11 09:11:58 +0530

    first commit

---
+++ src/main/java/com/geo/analizer/GeoAnalizerApplication.java
@@ -0,0 +1,14 @@
+package com.geo.analizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoAnalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoAnalizerApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/geo/analizer/GeoAnalizerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.geo.analizer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GeoAnalizerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
